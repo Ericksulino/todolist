aca1d6dd6339e503b6e4109a86b0b9d7647d1fa0
ericksulino <ericksulino@gmail.com>
2023-10-14 18:55:51 -0300

    last Commit

---
+++ src/main/java/com/ericksulino/todolist/errors/ExeptionHandlerController.java
@@ -0,0 +1,15 @@
+package com.ericksulino.todolist.errors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExeptionHandlerController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ResponseEntity.status(400).body(e.getMostSpecificCause().getMessage());
+    }
+}
